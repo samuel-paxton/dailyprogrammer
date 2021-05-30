4e23d8791f42f9ba74be048d68948013158956d5
Sam Paxton <samuel.m.paxton@gmail.com>
2021-05-29 21:00:16 -0400

    asdf

--- challenge388/main.cpp
+++ challenge388/main.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 #include <string>
 
+int nextpal(int pal) {
+    return 5;
+}
+
 int main() {
-    std::cout << " helo";
+    int pal; 
+
+    std::cout << "Enter palindrome: ";
+    std::cin >> pal;
+
+    std::cout << "The next smallest palindrome is: " << nextpal(pal) << "\n";
 }
